25609b81bf25404caffef94397e03a8398b4ccc4
Lumin Shi <shilumin@gmail.com>
2013-04-09 09:15:47 -0500

    update

--- src/com/collegeliving/Settings.java
+++ src/com/collegeliving/Settings.java
@@ -4,10 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,24 +18,79 @@ public class Settings extends Activity {
 		setContentView(R.layout.activity_settings_screen);
 		setLogoutBtn();
 		setEmailOnchange();
+		setRadiusOnchange();
+		setDisplayNameOnchange();
+		setPasswordOnchange();
 	}
 	
 	public void setEmailOnchange(){
 		EditText email_setting = (EditText) findViewById(R.id.Email_setting);
-		email_setting.addTextChangedListener(new TextWatcher() {
-	        @Override
-	        public void afterTextChanged(Editable s) {
-	            // TODO Auto-generated method stub
-	        }
-	        @Override
-	        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-	            // TODO Auto-generated method stub
-	        }
-	        @Override
-	        public void onTextChanged(CharSequence s, int start, int before, int count) {
-	        	Toast.makeText(null, "email changed!", Toast.LENGTH_LONG).show();
-	        } 
-	    });
+		email_setting.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+            	EditText email = (EditText) v;
+        		String oldemail = new String();
+            	if(!hasFocus){
+                	String newemail = email.getText().toString();
+                	if(!oldemail.equals(newemail)){
+                		Toast.makeText(getApplicationContext(), "email changed", Toast.LENGTH_SHORT).show();
+                	}
+                }else{
+                	oldemail = email.getText().toString();
+                }
+            }
+        });
+	}
+	public void setRadiusOnchange(){
+		EditText radius_setting = (EditText) findViewById(R.id.Radius_setting);
+		radius_setting.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+            	EditText radius = (EditText) v;
+            	String oldradius = new String();
+                if(!hasFocus){
+                	String newradius = radius.getText().toString();
+                	if(!oldradius.equals(newradius))
+                		Toast.makeText(getApplicationContext(), "radius changed", Toast.LENGTH_SHORT).show();
+                }else{
+                	oldradius = radius.getText().toString();
+                }
+            }
+        });
+	}
+	public void setDisplayNameOnchange(){
+		EditText displayname_setting = (EditText) findViewById(R.id.DisplayName_setting);
+		displayname_setting.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+            	EditText displayname = (EditText) v;
+            	String olddisplayname = new String();
+                if(!hasFocus){
+                    String newdisplayname = displayname.getText().toString();
+                    if(!olddisplayname.equals(newdisplayname))
+                    	Toast.makeText(getApplicationContext(), "displayname changed", Toast.LENGTH_SHORT).show();
+                }else{
+                	olddisplayname = displayname.getText().toString();
+                }
+            }
+        });
+	}
+	public void setPasswordOnchange(){
+		EditText password_setting = (EditText) findViewById(R.id.Password_setting);
+		password_setting.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+            	EditText pw = (EditText) v;
+            	String oldpw = new String();
+                if(!hasFocus){
+                    String newpw = pw.getText().toString();
+                    if(!oldpw.equals(newpw))
+                    	Toast.makeText(getApplicationContext(), "password changed", Toast.LENGTH_SHORT).show();
+                }else{
+                	oldpw = pw.getText().toString();
+                }
+            }
+        });
 	}
 	public void setLogoutBtn(){
 		Button LogoutBtn = (Button) findViewById(R.id.logout_button);
